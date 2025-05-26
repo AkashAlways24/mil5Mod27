c258e08b082d81215b05472d220f4e68ed9110a9
Arman Hossain Akash <armanhossainakash52@gmail.com>
2025-05-26 22:50:39 +0600

    login er kaj ses

--- script/logIn.js
+++ script/logIn.js
@@ -1,4 +1,22 @@
 const logInBTN = document.getElementById("logInBTN");
-logInBTN.addEventListener('click', function(){
-  
+logInBTN.addEventListener('click', function(event){
+  event.preventDefault();
+
+  const userID = document.getElementById('userID').value
+  // console.log(userID);
+  const userPin = document.getElementById('userPin').value
+  const convatePin = parseInt(userPin);
+  // console.log(userPin);
+
+  if( userID.length === 11){
+    if(userPin.length === 4){
+        window.location.href="./main.html"
+    }
+    else{
+      alert('Wrong Pin...........');
+    }
+  }
+  else{
+    alert('Input user number 11 and pin 4');
+  }
 });
